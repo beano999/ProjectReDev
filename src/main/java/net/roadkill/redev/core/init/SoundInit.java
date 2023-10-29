334ac773f100339a3538d630bdf5c7c87e0a9c65
MikulDev <gamer3mc@gmail.com>
2023-10-28 22:08:36 -0400

    Register lithican sounds

--- src/main/java/net/roadkill/redev/core/init/SoundInit.java
+++ src/main/java/net/roadkill/redev/core/init/SoundInit.java
@@ -13,4 +13,9 @@ public class SoundInit
 
     public static final RegistryObject<SoundEvent> PETRIFIED_DOOR_OPEN = SOUNDS.register("block.petrified_door.open", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ReDev.MOD_ID, "block.petrified_door.open")));
     public static final RegistryObject<SoundEvent> PETRIFIED_DOOR_CLOSE = SOUNDS.register("block.petrified_door.close", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ReDev.MOD_ID, "block.petrified_door.close")));
+
+    public static final RegistryObject<SoundEvent> LITHICAN_AMBIENT = SOUNDS.register("entity.lithican.ambient", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ReDev.MOD_ID, "entity.lithican.ambient")));
+    public static final RegistryObject<SoundEvent> LITHICAN_HURT = SOUNDS.register("entity.lithican.hurt", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ReDev.MOD_ID, "entity.lithican.hurt")));
+    public static final RegistryObject<SoundEvent> LITHICAN_DEATH = SOUNDS.register("entity.lithican.death", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ReDev.MOD_ID, "entity.lithican.death")));
+    public static final RegistryObject<SoundEvent> LITHICAN_AWAKEN = SOUNDS.register("entity.lithican.awaken", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ReDev.MOD_ID, "entity.lithican.awaken")));
 }
--- src/main/java/net/roadkill/redev/util/registries/ModSounds.java
+++ src/main/java/net/roadkill/redev/util/registries/ModSounds.java
@@ -7,4 +7,9 @@ public class ModSounds
 {
     public static final SoundEvent PETRIFIED_DOOR_OPEN = SoundInit.PETRIFIED_DOOR_OPEN.get();
     public static final SoundEvent PETRIFIED_DOOR_CLOSE = SoundInit.PETRIFIED_DOOR_CLOSE.get();
+
+    public static final SoundEvent LITHICAN_AMBIENT = SoundInit.LITHICAN_AMBIENT.get();
+    public static final SoundEvent LITHICAN_HURT = SoundInit.LITHICAN_HURT.get();
+    public static final SoundEvent LITHICAN_DEATH = SoundInit.LITHICAN_DEATH.get();
+    public static final SoundEvent LITHICAN_AWAKEN = SoundInit.LITHICAN_AWAKEN.get();
 }
