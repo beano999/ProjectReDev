3c6dde1fde93480e5770437de2652e98790d127d
MikulDev <gamer3mc@gmail.com>
2025-01-01 21:33:58 -0500

    Rework shield blocking behavior

--- src/main/java/net/roadkill/redev/common/event/ShieldBlock.java
+++ src/main/java/net/roadkill/redev/common/event/ShieldBlock.java
@@ -4,59 +4,53 @@ import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.ProjectileImpactEvent;
 import net.neoforged.neoforge.event.entity.living.LivingShieldBlockEvent;
 import net.roadkill.redev.core.init.ItemInit;
-import net.roadkill.redev.util.registries.ModItems;
 
 @EventBusSubscriber
 public class ShieldBlock
 {
-    @SubscribeEvent
-    public static void cancelProjectileHit(ProjectileImpactEvent event)
-    {
-        if (!(event.getRayTraceResult() instanceof EntityHitResult entityHit)) return;
-        if (!(entityHit.getEntity() instanceof LivingEntity entity)) return;
-        if (entity.isUsingItem() && entity.getUseItem().is(ItemInit.INFERNAL_PLATE)
-        && entity.isDamageSourceBlocked(entity.damageSources().mobProjectile(event.getProjectile(), null)))
-        {   //event.setCanceled(true);
-        }
-    }
-
     @SubscribeEvent
     public static void blockDamage(LivingShieldBlockEvent event)
     {
         if (!event.getOriginalBlock()) return;
 
         LivingEntity entity = event.getEntity();
+        DamageSource damageSource = event.getDamageSource();
 
-        // Halve damage blocked by Vanilla shield
-        if (entity.getUseItem().is(Items.SHIELD))
+        // Explosions always penetrate for half damage
+        if (damageSource.is(DamageTypes.FIREWORKS) || damageSource.is(DamageTypes.EXPLOSION) || damageSource.is(DamageTypes.PLAYER_EXPLOSION))
         {   event.setBlockedDamage(event.getBlockedDamage() / 2);
+            return;
         }
+        if (entity instanceof Player player)
+        {
+            float cooldown = player.getCooldowns().getCooldownPercent(entity.getUseItem(), 0);
+            if (cooldown > 0)
+            {   event.setBlockedDamage(event.getBlockedDamage() / 2);
+            }
+            else player.getCooldowns().addCooldown(player.getItemInHand(entity.getUsedItemHand()), 20);
+        }
+
         // The rest of the logic is for the Inferno Plate
         if (!entity.getUseItem().is(ItemInit.INFERNAL_PLATE))
         {   return;
         }
-        // Protection from direct attacks is halved again
-        if (event.getDamageSource().is(DamageTypes.MOB_ATTACK) || event.getDamageSource().is(DamageTypes.PLAYER_ATTACK))
-        {   event.setBlockedDamage(event.getBlockedDamage() / 2);
-        }
         // The rest of the logic is for reflecting projectiles
-        if (!(event.getDamageSource().getDirectEntity() instanceof Projectile projectile) || event.getDamageSource().getEntity() == null)
+        if (!(damageSource.getDirectEntity() instanceof Projectile projectile) || damageSource.getEntity() == null)
         {   return;
         }
 
@@ -71,7 +65,7 @@ public class ShieldBlock
             level.sendParticles(ParticleTypes.FLAME, target.x, target.y, target.z, 1, 0.2, 0.2, 0.2, 0.05);
         }
 
-        Entity shooter = event.getDamageSource().getEntity();
+        Entity shooter = damageSource.getEntity();
 
         // Calculate the direction to the shooter
         Vec3 arrowPosition = projectile.position();
