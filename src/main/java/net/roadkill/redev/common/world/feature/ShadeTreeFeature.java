b18430f96cac474950fa7fc2034dbb50d698edf0
MikulDev <gamer3mc@gmail.com>
2025-01-01 23:46:23 -0500

    Alternate shade tree placement that doesn't replace blocks

--- src/main/java/net/roadkill/redev/common/world/feature/ShadeTreeFeature.java
+++ src/main/java/net/roadkill/redev/common/world/feature/ShadeTreeFeature.java
@@ -5,9 +5,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
-import net.neoforged.neoforge.common.Tags;
 import net.roadkill.redev.common.block.ShadeSaplingBlock;
 import net.roadkill.redev.common.world.tree.ShadeTree;
+import net.roadkill.redev.core.init.BlockInit;
 
 public class ShadeTreeFeature extends Feature<NoneFeatureConfiguration>
 {
@@ -19,17 +19,15 @@ public class ShadeTreeFeature extends Feature<NoneFeatureConfiguration>
     public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> placement)
     {
         BlockState blockAt = placement.level().getBlockState(placement.origin());
-        boolean isSaplingAt;
         ShadeTree.Color color;
         if (blockAt.getBlock() instanceof ShadeSaplingBlock sapling)
         {   color = sapling.getColor();
-            isSaplingAt = true;
         }
         else
         {   color = ShadeTree.Color.NORMAL;
-            isSaplingAt = false;
         }
-        return (placement.level().getBlockState(placement.origin().below()).is(Tags.Blocks.SANDS) || isSaplingAt)
+        BlockState belowState = placement.level().getBlockState(placement.origin().below());
+        return BlockInit.SHADE_SAPLING.value().canSurvive(belowState, placement.level(), placement.origin())
         && ShadeTree.place(placement.level(), placement.origin(), color);
     }
 }
--- src/main/java/net/roadkill/redev/common/world/tree/ShadeTree.java
+++ src/main/java/net/roadkill/redev/common/world/tree/ShadeTree.java
@@ -1,14 +1,17 @@
 package net.roadkill.redev.common.world.tree;
 
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.JigsawBlock;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import net.roadkill.redev.ReDev;
+import net.roadkill.redev.common.block.ShadeSaplingBlock;
 
 import java.util.Optional;
 
@@ -27,30 +30,65 @@ public class ShadeTree
             case PURPLE -> "purple_";
         };
         Rotation rotation = Rotation.getRandom(level.getRandom());
-        Optional<StructureTemplate> treeOpt = structuretemplatemanager.get(ResourceLocation.fromNamespaceAndPath(ReDev.MOD_ID, "shade_tree/" + color + "shade_tree_" + treeVariant));
+        Optional<StructureTemplate> tree = structuretemplatemanager.get(ResourceLocation.fromNamespaceAndPath(ReDev.MOD_ID, "shade_tree/" + color + "shade_tree_" + treeVariant));
 
-        treeOpt.ifPresent(tree ->
-        {   Pair<Integer, Integer> offset = getRotatedOffset(treeVariant, rotation);
-            tree.placeInWorld(level, pos.offset(offset.getFirst(), 0, offset.getSecond()), pos, new StructurePlaceSettings().setRotation(rotation), level.getRandom(), 2);
-        });
-        return true;
+        if (tree.isPresent())
+        {
+            // Get block palette for tree variant
+            StructurePlaceSettings randRotationSettings = new StructurePlaceSettings().setRotation(rotation);
+            StructureTemplate.Palette palette = randRotationSettings.getRandomPalette(tree.get().palettes, origin.offset(getRotatedOffset(treeVariant, rotation)));
+
+            BlockPos treePos = origin.offset(getOffset(treeVariant));
+            for (StructureTemplate.StructureBlockInfo blockInfo : palette.blocks())
+            {
+                if (blockInfo.state().getBlock() instanceof JigsawBlock)
+                {
+                    treePos = pos.set(origin).move(blockInfo.pos());
+                    break;
+                }
+            }
+
+            for (StructureTemplate.StructureBlockInfo blockInfo : palette.blocks())
+            {
+                BlockState state = blockInfo.state();
+                pos.set(treePos).move(blockInfo.pos());
+                BlockState blockAt = level.getBlockState(pos);
+                if (blockAt.canBeReplaced() || blockAt.getBlock() instanceof ShadeSaplingBlock)
+                {   level.setBlock(pos, state, 3);
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
-    public static Pair<Integer, Integer> getRotatedOffset(int variant, Rotation rotation)
+    public static BlockPos getOffset(int variant)
     {
-        Pair<Integer, Integer> offset = switch (variant)
+        return switch (variant)
         {
-            case 1 -> Pair.of(-6, -7);
-            case 2 -> Pair.of(-8, -5);
-            case 3 -> Pair.of(-8, -6);
-            case 4 -> Pair.of(-7, -6);
-            default -> Pair.of(-5, -6);
+            case 1 -> new BlockPos(-6, 0, -7);
+            case 2 -> new BlockPos(-8, 0, -5);
+            case 3 -> new BlockPos(-8, 0, -6);
+            case 4 -> new BlockPos(-7, 0, -6);
+            default -> new BlockPos(-5, 0, -6);
+        };
+    }
+
+    public static Vec3i getRotatedOffset(int variant, Rotation rotation)
+    {
+        Vec3i offset = switch (variant)
+        {
+            case 1 -> new Vec3i(-6, 0, -7);
+            case 2 -> new Vec3i(-8, 0, -5);
+            case 3 -> new Vec3i(-8, 0, -6);
+            case 4 -> new Vec3i(-7, 0, -6);
+            default -> new Vec3i(-5, 0, -6);
         };
         return switch (rotation)
         {
-            case CLOCKWISE_90 -> Pair.of(-offset.getSecond(), offset.getFirst());
-            case CLOCKWISE_180 -> Pair.of(-offset.getFirst(), -offset.getSecond());
-            case COUNTERCLOCKWISE_90 -> Pair.of(offset.getSecond(), -offset.getFirst());
+            case CLOCKWISE_90 -> new Vec3i(offset.getZ(), offset.getY(), -offset.getX());
+            case CLOCKWISE_180 -> new Vec3i(-offset.getX(), offset.getY(), -offset.getZ());
+            case COUNTERCLOCKWISE_90 -> new Vec3i(-offset.getZ(), offset.getY(), offset.getX());
             default -> offset;
         };
     }
